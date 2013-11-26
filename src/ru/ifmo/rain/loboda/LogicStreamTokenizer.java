2467c5ff6927a25ffab1116d7702fea6ea816ef1
Loboda Aleksandr <loboda@rain.ifmo.ru>
2013-11-26 11:37:31 +0400

    Tokenizer was written

--- src/ru/ifmo/rain/loboda/LogicStreamTokenizer.java
+++ src/ru/ifmo/rain/loboda/LogicStreamTokenizer.java
@@ -6,16 +6,15 @@ import java.io.InputStream;
 import java.io.PushbackInputStream;
 
 public class LogicStreamTokenizer {
-    public char variableName;
+    private char[] name;
     private PushbackInputStream stream;
-    private int line = 1;
 
     public LogicStreamTokenizer(InputStream stream) {
         this.stream = new PushbackInputStream(new BufferedInputStream(stream));
     }
 
-    public int getLine() {
-        return line;
+    char[] get_name(){
+        return name;
     }
 
     public Token nextToken() throws IOException, ParserException {
@@ -27,6 +26,10 @@ public class LogicStreamTokenizer {
             return Token.END;
         }
         switch (ch) {
+            case '?':
+                return Token.EXISTENCE;
+            case '@':
+                return Token.UNIVERSAL;
             case '&':
                 return Token.AND;
             case '|':
@@ -41,28 +44,33 @@ public class LogicStreamTokenizer {
             case '!':
                 return Token.NOT;
             case '\n':
-                ++line;
                 return Token.PRINT;
             case ',':
                 return Token.COMMA;
             case '-':
-                ch = stream.read();
-                if (ch == '>') {
-                    return Token.IMPLICATION;
-                } else {
-                    throw new ParserException("Expected ->", line);
-                }
+                stream.read();
+                return Token.IMPLICATION;
             case '(':
                 return Token.LP;
             case ')':
                 return Token.RP;
             default:
-                if (ch >= 'A' && ch <= 'Z') {
-                    variableName = (char) ch;
-                    return Token.VARIABLE;
+                Token token;
+                if(ch >= 'a' && ch <= 'z'){
+                    token = Token.TERM;
                 } else {
-                    throw new ParserException("Unrecognized token", line);
+                    token = Token.PREDICATE;
                 }
+                int first = ch;
+                ch = stream.read();
+                if(ch >= '0' && ch <= '9'){
+                    name = new char[]{(char)first, (char)ch};
+                }
+                if(ch != -1){
+                    stream.unread(ch);
+                }
+                name = new char[]{(char)first};
+                return token;
         }
     }
 }
